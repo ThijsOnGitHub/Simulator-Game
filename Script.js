220269a1d3e6973a7908cf28fb92dc786db7cbe9
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-02-05 18:28:50 +0100

    Kleurenknop veranderen

---
+++ Script.js
@@ -0,0 +1,15 @@
+var startButton=document.getElementById("startButton")
+
+
+function randomInt(min,max) {
+    return Math.floor(Math.random()*((max+1)-min))+min
+}
+function changingColor(or,og,ob){
+    r= randomInt(0,255)
+    g= randomInt(0,255)
+    b= randomInt(0,255)
+
+    
+}
+setTimeout{,5000}
+ 
