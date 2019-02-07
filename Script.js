641013d0781c3425d077826b1cfbed8d171e50e6
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-02-07 22:02:55 +0100

    Better working wachtrij

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ var startButton=document.getElementById("startKnop")
 var disapear=document.getElementById("disapear")
 var show=document.getElementById("show")
 var mensen=document.getElementById("mensen")
-
+var wachtrijen=document.getElementById("wachtrijen")
 function randomInt(min,max) {
     return Math.floor(Math.random()*((max+1)-min))+min
 };
@@ -12,7 +12,7 @@ startButton.addEventListener("click",function(){
     show.style.display="block"
     startGame()
 }) ;
-startGame()
+
 
 function mensenSpawn(id){
     skins=["Geel","Groen","Oranje","Rood"]
@@ -36,15 +36,27 @@ function walk(naam,id){
         if(Number(naam.style.left.replace("px",""))+25>500){
             mensen.removeChild(naam)
             clearInterval(walkLoop[id])
-        }else{
-            console.log(Number(naam.style.left.replace("px","")))
-            console.log(Number(naam.style.left.replace("px",""))>500)
         }
 
     },10)
 }
+// positielijst in de vorm [[x,y],[x,y]
+function maakWachtrij(breedte,positieLijst){
+    for (let index = 0; index < positieLijst.length-1; index++) {
+        var posities=[positieLijst[index]].concat([positieLijst[index+1]]).sort(function(a,b){if(a[0]==b[0]){return a[1]-b[1]}else{return a[0]-b[0]}})
+        console.log(posities)
+        var rij=document.createElement("div")
+        rij.id=index
+        rij.style.top= posities[0][1]-breedte
+        rij.style.left= posities[0][0]
+        rij.style.width= posities[1][0]-posities[0][0]+(breedte*  (posities[1][0]==posities[0][0]?1 : 2 ))
+        rij.style.height=posities[1][1]-posities[0][1]+(breedte*  (posities[1][0]==posities[0][0]?2 : 1 ))
+        wachtrijen.appendChild(rij)
+    }
+}
 
 function startGame(){
     mensenSpawn(0)
-    gameLoop()
+    maakWachtrij(30,[[50,60],[100,60],[100,120],[150,120]])
 }
+startGame()
