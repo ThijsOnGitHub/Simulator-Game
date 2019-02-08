1b2a40798e92eb04da5d4efb761e56997afb62e7
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-02-08 13:46:58 +0100

    Converter werkt weer

--- Script.js
+++ Script.js
@@ -14,9 +14,12 @@ startButton.addEventListener("click",function(){
 }) ;
 //the game
 
-var wachtrij=maakXYLijst([[70,0],[0,50]],[20,40])
 
-function mensenSpawn(id){
+var startWachtrij=[20,40]
+var wachtrijInstructies=[[70,0],[0,70]]
+var wachtrij=maakXYLijst(wachtrijInstructies,startWachtrij)
+
+function mensenSpawn(id,startwachtrij,wachtrijInstructies){
     skins=["Geel","Groen","Oranje","Rood"]
     var persoon=document.createElement("img")
     naam="persoon"+toString(id)
@@ -24,20 +27,31 @@ function mensenSpawn(id){
     persoon.src="./imgs/Poppetje "+skins[randomInt(0,3)]+".png"
     persoon.style.transform="rotate(90deg)"
     persoon.style.position="absolute"
-    persoon.style.left=10
-    persoon.style.top=10
-    mensen.appendChild(persoon,naam)
-    walk(persoon,naam)
-    setTimeout(mensenSpawn,/*randomInt(1000,5000)*/10,id++)
+    persoon.style.left=startWachtrij[0]
+    persoon.style.top=startWachtrij[1]
+    mensen.appendChild(persoon)
+    walk(persoon,naam,startwachtrij,wachtrijInstructies)
+    setTimeout(mensenSpawn,randomInt(1000,5000),id++)
 }
 
-function walk(naam,id){
+function walk(naam,id,wachtrijInstructies){
     var walkLoop=[]
     naam.style.left=wachtrij[0][0]
     naam.style.top=wachtrij[0][1]
     walkLoop[id]=setInterval(function(){
+        /*
+        instructie=wachtrijInstructies[1]
+
+        if (instructie[0]==0 && instructie[1]==0){
+            wachtrijInstructies=wachtrijInstructies.slice()
+        }else{
+            wachtrijInstructies=wachtrijInstructies.slice(1,wachtrijInstructies.length)
+            console.log(wachtrijInstructies)
+        }
+        */
         naam.style.left=Number(naam.style.left.replace("px",""))+1
-        
+
+
         if(Number(naam.style.left.replace("px",""))+25>500){
             mensen.removeChild(naam)
             clearInterval(walkLoop[id])
@@ -45,11 +59,10 @@ function walk(naam,id){
 
     },10)
 }
-// positielijst in de vorm [[x,y],[x,y]
+// positielijst in de vorm [[x,y],[x,y]]
 function maakWachtrij(breedte,positieLijst){
     for (let index = 0; index < positieLijst.length-1; index++) {
         var posities=[positieLijst[index]].concat([positieLijst[index+1]]).sort(function(a,b){if(a[0]==b[0]){return a[1]-b[1]}else{return a[0]-b[0]}})
-        console.log(posities)
         var rij=document.createElement("div")
         rij.id=index
         rij.style.top= posities[0][1]-10
@@ -67,20 +80,19 @@ function maakXYLijst(actielijst,begin){
         if (actielijst[index-1]==undefined){
             vorigItem=begin
         }else{
-            vorigItem=nieuweLijst[nieuweLijst.length-1]
+            vorigItem=actielijst[index-1]
         }
-        console.log(vorigItem)
-        actie=actielijst[index]
-        nieuweX=vorigItem[0]+actie[0]
-        nieuweY=vorigItem[1]+actie[1]
+        var actie=actielijst[index]
+        var nieuweX=vorigItem[0]+actie[0]
+        var nieuweY=vorigItem[1]+actie[1]
         nieuweLijst.push([nieuweX,nieuweY])
     }
     return nieuweLijst
 }
 
 
-function startGame(){
-    mensenSpawn(0)
+function startGame(wachtrij,startWachtrij,wachtrijInstructies){
+    mensenSpawn(0,startWachtrij,wachtrijInstructies)
     maakWachtrij(40,wachtrij)
 }
-startGame()
+startGame(wachtrij,startWachtrij,wachtrijInstructies)
